d17c1171382cb50b5e48f3307c26d2fa8b919d7b
bigbetroyal-lab <bigbetroyal@gmail.com>
2026-01-01 23:02:14 +0000

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/dist/supabase.mjs";
+
+export const supabase = createClient(
+  "https://aplaqtdpbjzirtsybgyc.supabase.co",
+  "sb_publishable_J0Wu4wGxaRxwqIFNdiYnJA_pmzx59_O"
+);
